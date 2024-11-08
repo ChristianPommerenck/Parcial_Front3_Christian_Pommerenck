3500b2a30e3df5e1304c7c087368aec63cd698e8
ChristianPommerenck <christian.pommerenck@gmail.com>
2024-11-07 23:13:50 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Card from './Components/Card'
+import './styles/App.css'
+
+function App() {
+  return (
+    <div className='App'>
+      <h1>Carga de estudiantes</h1>
+      <form></form>
+      <Card/>
+    </div>
+  );
+}
+
+export default App
